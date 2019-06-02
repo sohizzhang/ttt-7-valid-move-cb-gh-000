db6c4b96aaa2901b459fec06b59afa3d3d91798b
S Yoon <sohizzhang@gmail.com>
2019-06-02 15:32:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,13 @@
 def valid_move?(board, index)
   index = gets.to_i.chomp - 1
-  if index.inbetween?(0, 8) && position?(board,index) == false
+  if index.inbetween?(0, 8) && position_taken?(board,index) == false
     true
   else
     false
   end
 end
 
-def position?(board, index)
+def position_taken?(board, index)
   value = board[index]
   if value == " " || value == "" || value == nil
     false
