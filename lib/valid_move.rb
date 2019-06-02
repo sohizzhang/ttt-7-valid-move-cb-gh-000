4d812aa75b30581409be88e0b17ea75271610c07
S Yoon <sohizzhang@gmail.com>
2019-06-02 15:16:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,15 +1,7 @@
 def valid_move?(board, index)
   index = index - 1
-  if index == index.inbetween?(0, 8)
-    true
-  else
-    false
-  end
-end
-
-def position?(board, index)
   value = board[index]
-  if value == " " || value == "" || value == nil
+  if (index == index.inbetween?(0, 8)) && (value == " " || value == "" || value == nil)
     true
   else
     false
